e56d7a4b78de256029d059420b0d5bef37e38295
Duc Hoang Manh <149278126+Jandyz-1@users.noreply.github.com>
2024-08-20 20:47:17 +0700

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float f1 = 35e1f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+        int x = 12;
+        
+    }
+}
